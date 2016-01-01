71b4993c8dcc017439232e353e4d255cc522e08e
AdrianDC <radian.dc@gmail.com>
2016-01-01 15:21:52 +0100

    Settings: Apps started on boot shortcut in memory settings
     * Implement an intent extra to retrieve a tab target
     * Use a tab identifer from apps_ops_categories_cm
        to make sure the selection is correct or ignored
    
    Change-Id: I79d963ecbadc624dc45a398387a348691318b6be
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -149,9 +149,18 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         }
         filterPageData(pageData, specificTab);
 
+        int defaultTab = -1;
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            defaultTab = Arrays.asList(mPageNames).indexOf(bundle.getString("appops_tab", ""));
+        }
+
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         mAdapter = new MyPagerAdapter(getChildFragmentManager(), pageData);
         mViewPager.setAdapter(mAdapter);
+        if (defaultTab >= 0) {
+            mViewPager.setCurrentItem(defaultTab);
+        }
         mViewPager.setOnPageChangeListener(mAdapter);
         PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
 
